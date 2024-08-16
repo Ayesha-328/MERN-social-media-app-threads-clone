230ef451c2fdf45d6f7d9c346400784321cf6930
Ayesha Najeeb <ayeshanajeeb2803@gmail.com>
2024-08-16 22:21:55 +0500

    deployment changes added

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 import userRoutes from "./routes/userRoutes.js"
@@ -16,6 +17,7 @@ connectDB();
 
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 cloudinary.config({
   cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
@@ -39,5 +41,18 @@ app.use(cookieParser()); //Allow us to get the cookie from req and set the cooki
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/messages", messageRoutes)
+
+
+// http://localhost:5000 =>server,frontend
+
+if(process.env.NODE_ENV === "production"){
+  // converting in static asset
+  app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+  // react app
+  app.get("*", (req,res)=> {
+    res.sendFile(path.resolve(__dirname,"frontend","dist", "index.html"))
+  })
+}
 
 server.listen(PORT, ()=>console.log(`Server started at http://localhost:${PORT}`))
