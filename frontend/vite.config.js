8ed246733e5b9c95f35aea2f39455bb9bd29f095
Ayesha Najeeb <ayeshanajeeb2803@gmail.com>
2024-08-17 22:46:33 +0500

    deployment changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
 	plugins: [react()],
 	server: {
 		port: 3000,
@@ -17,18 +16,5 @@ export default defineConfig({
 		},
 	},
 });
-=======
-  plugins: [react()],
-  server:{
-    port:3000,
-    // to get rid of the cors errors
-    proxy:{
-      "/api" : {
-        target : "https://mern-social-media-app-threads-clone.vercel.app",
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
-})
->>>>>>> d1ea470bcc1bf15b57781169efb287c452696049
+
+
--- server/server.js
+++ server/server.js
@@ -22,23 +22,12 @@ cloudinary.config({
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-<<<<<<< HEAD
+
 // Middlewares
 app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
-=======
-// Middlewares => The funcs that run between req and res
-app.use(cors({
-  origin: 'https://mern-social-media-app-threads-clone.vercel.app',
-  credentials: true,
-}));
 
-app.use(express.json({ limit: '10mb' })); // Adjust limit as needed
-app.use(express.urlencoded({ extended: true, limit: '10mb' })); // Adjust limit as needed
-app.use(cookieParser()); //Allow us to get the cookie from req and set the cookie from res
-
->>>>>>> d1ea470bcc1bf15b57781169efb287c452696049
 
 // Routes
 app.use("/api/users", userRoutes);
@@ -56,8 +45,4 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 
-<<<<<<< HEAD
 server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
-=======
-server.listen(PORT, ()=>console.log(`Server started at http://localhost:${PORT}`))
->>>>>>> d1ea470bcc1bf15b57781169efb287c452696049
